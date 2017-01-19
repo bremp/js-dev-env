5a08be1517babb4e65cd271f315ac5b10c264f8b
Jayneson <jayneson@gmail.com>
2017-01-18 20:40:18 -1000

    Added module format using ES6 modules and bundling using webpack.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// Using ES6 syntax and module loader
+
+import "./index.css";
+
+import numeral from "numeral";
+
+const courseValue = numeral(1000).format("$0,0.00");
+debugger;
+console.log(`I would pay ${courseValue} for this awesome course!`);
